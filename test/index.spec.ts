c7ae929acd7c52a0ee140981c0a1c25691436cfe
adrigzr <adrigzr@users.noreply.github.com>
2021-03-04 11:11:50 +0100

    chore: add more linters

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,9 @@
-import { describe, it } from 'mocha';
-import { helloWorld } from '../src/index';
-import { expect } from 'chai';
+import { describe, it } from "mocha";
+import { helloWorld } from "../src/index";
+import { expect } from "chai";
 
-describe('index', () => {
-  it('works', () => {
+describe("index", () => {
+  it("works", () => {
     expect(helloWorld).to.exist;
-  })
-})
+  });
+});
