02884fde2059f881d4491ef32b1ae249c7f4032d
adrigzr <adrigzr@users.noreply.github.com>
2021-08-16 14:34:25 +0200

    fix(core): set battery value interpolation to 100..200

--- packages/core/src/mappers/device-battery.mapper.ts
+++ packages/core/src/mappers/device-battery.mapper.ts
@@ -1,16 +1,35 @@
 import { Mapper } from "../base-classes/mapper.base";
+import { interpolate } from "../utils/interpolate.util";
 import { DeviceBattery } from "../value-objects/device-battery.value-object";
 
 const ROBOT_MAX_VALUE = 200;
+const ROBOT_MIN_VALUE = 100;
+
+const ROBOT = {
+  min: ROBOT_MIN_VALUE,
+  max: ROBOT_MAX_VALUE,
+};
+const DEVICE = {
+  min: DeviceBattery.MIN_VALUE,
+  max: DeviceBattery.MAX_VALUE,
+};
 
 export const DeviceBatteryMapper: Mapper<DeviceBattery, number> = class {
   static toDomain(battery: number): DeviceBattery {
+    if (battery < ROBOT_MIN_VALUE) {
+      battery = ROBOT_MIN_VALUE;
+    }
+
+    if (battery > ROBOT_MAX_VALUE) {
+      battery = ROBOT_MAX_VALUE;
+    }
+
     return new DeviceBattery({
-      value: (battery * DeviceBattery.MAX_VALUE) / ROBOT_MAX_VALUE,
+      value: interpolate(battery, ROBOT, DEVICE),
     });
   }
 
   static toRobot(battery: DeviceBattery): number {
-    return (battery.value * ROBOT_MAX_VALUE) / DeviceBattery.MAX_VALUE;
+    return Math.floor(interpolate(battery.value, DEVICE, ROBOT));
   }
 };
