2fb9b85a66c832740421e42eda7e2f543a7f24f1
adrigzr <a.gonzalezrus@gmail.com>
2023-02-10 22:02:19 +0100

    chore: unify eslint config in a package

---
+++ packages/eslint-config/javascript.js
@@ -0,0 +1,18 @@
+module.exports = {
+  parserOptions: {
+    sourceType: "script",
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:node/recommended",
+    "plugin:security/recommended",
+    "plugin:import/recommended",
+    "plugin:prettier/recommended",
+  ],
+  settings: {
+    node: {
+      allowModules: ["@agnoc/core", "@agnoc/cli"],
+      tryExtensions: [".js", ".ts", ".json"],
+    },
+  },
+};
