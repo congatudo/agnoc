1c77fba642e01d6c592aeb3e9d4d0ec133d7a0a0
adrigzr <a.gonzalezrus@gmail.com>
2023-02-11 17:43:22 +0100

    refactor(toolkit): remove `exception.constant`

--- packages/toolkit/src/exceptions/argument-out-of-range.exception.ts
+++ packages/toolkit/src/exceptions/argument-out-of-range.exception.ts
@@ -1,6 +1,3 @@
 import { Exception } from '../base-classes/exception.base';
-import { Exceptions } from '../constants/exception.constant';
 
-export class ArgumentOutOfRangeException extends Exception {
-  readonly name = Exceptions.argumentOutOfRange;
-}
+export class ArgumentOutOfRangeException extends Exception {}
